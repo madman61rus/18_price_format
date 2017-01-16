29752b7b93e448df5b196297a99ff078d1c47d67
madman61rus <rhjnhjtn3yjhe>
2017-01-16 21:23:55 +0300

    Инициализирующий коммит : + Создан файл format_price.py с программой форматирования цены + Создан файл tests.py, который содержит тесты для проверки программы + Создан файл описания репозитория README.md

--- format_price.py
+++ format_price.py
@@ -1,5 +1,35 @@
+import re
+import argparse
+
 def format_price(price):
-    pass
+
+    if not isinstance(price,(str,float,int)):
+        raise TypeError('Не правильный формат данных')
+
+    if isinstance(price,str):
+        if not re.match('\d+(\.)?\d+$',price):
+            raise ValueError('Ошибка в значении цены')
+        price = string_to_numeric(price)
+
+    if isinstance(price,float):
+        return '{:,.2f}'.format(price).replace(',',' ')
+
+    if isinstance(price,int):
+        return '{:,}'.format(price).replace(',', ' ')
+
+
+def string_to_numeric(value):
+    try:
+        if value.find('.') >= 0:
+            return float(value)
+        else :
+            return int(value)
+    except:
+        return None
 
 if __name__ == '__main__':
-    pass
+    args = argparse.ArgumentParser()
+    args.add_argument('-p','--price', required=True, help='Value of price')
+    arguments = args.parse_args()
+
+    print(format_price(arguments.price))
--- tests.py
+++ tests.py
@@ -1,1 +1,39 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+class TestFormatString(unittest.TestCase):
+
+    def test_integer_value(self):
+        self.assertEqual(format_price(102343455444),'102 343 455 444','Integer value is FAIL')
+
+    def test_float_value(self):
+        self.assertEqual(format_price(102343455.444), '102 343 455.44', 'Float value is FAIL')
+
+    def test_string_int_value(self):
+        self.assertEqual(format_price("7856322152"), '7 856 322 152', 'Integer value in string is FAIL')
+
+    def test_string_float_value(self):
+        self.assertEqual(format_price("12856325.2132"), '12 856 325.21', 'Float value in string is FAIL')
+
+    def test_string_is_not_number(self):
+        with self.assertRaises(ValueError):
+            format_price("blablabla")
+
+    def test_type_value_error(self):
+        with self.assertRaises(ValueError):
+            format_price('234#23443!34@')
+
+    def test_comma_value_error(self):
+        with self.assertRaises(ValueError):
+            format_price('256835,465465')
+
+    def test_multi_point_value_error(self):
+        with self.assertRaises(ValueError):
+            format_price('452.253.635')
+
+    def test_negative_value_error(self):
+        with self.assertRaises(ValueError):
+            format_price('-256835.465465')
+
+if __name__ == '__main__':
+    unittest.main()
