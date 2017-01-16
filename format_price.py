88fb17c85c6285354a66157d828464c4ca20089f
madman61rus <rhjnhjtn3yjhe>
2017-01-16 21:26:12 +0300

    + Добавлен тип исключения в функции string_to_numeric

--- format_price.py
+++ format_price.py
@@ -24,7 +24,7 @@ def string_to_numeric(value):
             return float(value)
         else :
             return int(value)
-    except:
+    except ValueError:
         return None
 
 if __name__ == '__main__':
